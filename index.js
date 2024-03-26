e9c1682551a51c9bc1e15d60a09851fb9a555bd9
aizamalik1234 <aizamalik6789@gmail.com>
2024-03-26 21:58:50 +0500

    Adding a smpl CLI calculator project

---
+++ index.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// printing a wellcom message
+console.log("\n\tWellcome to \`maliksab\` - CLI smpl calclulator\n");
+// asking questions for user throuhg inquirer
+let answers = await inquirer.prompt([
+    { message: "enter first Number", type: "number", name: "firstNumber" },
+    { message: "enter second Number", type: "number", name: "secondNumber" },
+    {
+        message: "select one operator to perform operators",
+        type: "list",
+        name: "operators",
+        choices: ["Additions", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+console.log(answers);
+// condtional statments If-Else
+if (answers.operators === "Additions") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operators === "Subtraction") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operators === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operators === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
